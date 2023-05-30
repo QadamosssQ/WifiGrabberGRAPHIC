88d338c1d0988f81539a7f87870b2f653635f35c
Adamoss <95488005+QadamosssQ@users.noreply.github.com>
2023-05-30 23:38:32 +0200

    Add working code
    It works! without advanced option

--- src/MainWindow.xaml.cs
+++ src/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System;
+using System.Diagnostics;
 
 namespace testWpf
 {
@@ -9,6 +11,8 @@ namespace testWpf
         public MainWindow()
         {
             InitializeComponent();
+            LoadDetectedNetworks();
+
             TitleLabel.MouseLeftButtonDown += TitleLabel_MouseLeftButtonDown;
             TitleLabel.MouseMove += TitleLabel_MouseMove;
             TitleLabel.MouseLeftButtonUp += TitleLabel_MouseLeftButtonUp;
@@ -58,41 +62,141 @@ namespace testWpf
             if (WindowState == WindowState.Maximized)
             {
                 WindowState = WindowState.Normal;
-                Label_WifiList.FontSize = 20;
-                Label_Password.FontSize = 20;
-                Label_Password.FontSize = 20;
-                TextBox_Password.FontSize = 15;
+                LabelWifiList.FontSize = 20;
+                LabelPassword.FontSize = 20;
+                LabelPassword.FontSize = 20;
+                TextBoxPassword.FontSize = 15;
             }
             else
             {
                 WindowState = WindowState.Maximized;
-                Label_WifiList.FontSize = 50;
-                Label_Password.FontSize = 50;
-                Label_Password.FontSize = 50;
-                TextBox_Password.FontSize = 35;
+                LabelWifiList.FontSize = 50;
+                LabelPassword.FontSize = 50;
+                LabelPassword.FontSize = 50;
+                TextBoxPassword.FontSize = 35;
             }
         }
 
         private void btn_hover_advanced(object sender, MouseEventArgs e)
         {
-            Button_Advanced.Foreground = new System.Windows.Media.SolidColorBrush(System.Windows.Media.Color.FromRgb(70, 16, 118)); 
-            
+            ButtonAdvanced.Foreground =
+                new System.Windows.Media.SolidColorBrush(System.Windows.Media.Color.FromRgb(70, 16, 118));
         }
 
         private void btn_unhover_advanced(object sender, MouseEventArgs e)
         {
-            Button_Advanced.Foreground = new System.Windows.Media.SolidColorBrush(System.Windows.Media.Color.FromRgb(255, 255, 255));
+            ButtonAdvanced.Foreground =
+                new System.Windows.Media.SolidColorBrush(System.Windows.Media.Color.FromRgb(255, 255, 255));
         }
-        
+
         private void btn_hover_grab(object sender, MouseEventArgs e)
         {
-            Button_Grab.Foreground = new System.Windows.Media.SolidColorBrush(System.Windows.Media.Color.FromRgb(70, 16, 118)); 
-            
+            ButtonGrab.Foreground =
+                new System.Windows.Media.SolidColorBrush(System.Windows.Media.Color.FromRgb(70, 16, 118));
         }
 
         private void btn_unhover_grab(object sender, MouseEventArgs e)
         {
-            Button_Grab.Foreground = new System.Windows.Media.SolidColorBrush(System.Windows.Media.Color.FromRgb(255, 255, 255));
+            ButtonGrab.Foreground =
+                new System.Windows.Media.SolidColorBrush(System.Windows.Media.Color.FromRgb(255, 255, 255));
+        }
+
+
+        private void LoadDetectedNetworks()
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo
+            {
+                FileName = "netsh",
+                Arguments = "wlan show profile",
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            Process process = new Process
+            {
+                StartInfo = startInfo
+            };
+
+            process.Start();
+            string output = process.StandardOutput.ReadToEnd();
+            process.WaitForExit();
+
+            string[] lines = output.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+            int a = 0;
+            string[] sieci = new string[lines.Length];
+
+            foreach (string line in lines)
+            {
+                if (line.Contains("All User Profile"))
+                {
+                    a++;
+                    string linia_1 = line.Replace("All User Profile     : ", "");
+                    string linia_2 = linia_1.Substring(4);
+                    sieci[a] = linia_2;
+                }
+            }
+
+            if (a > 0)
+            {
+                for (int i = 1; i <= a; i++)
+                {
+                    NetworkComboBox.Items.Add(sieci[i]);
+                }
+            }
+        }
+
+
+        private void Button_Grab_OnClick(object sender, RoutedEventArgs e)
+        {
+            string selectedNetwork = (string)NetworkComboBox.SelectedItem;
+            if (selectedNetwork != null)
+            {
+                TextBoxPassword.Text = Show_wifi_password(selectedNetwork);
+            }
+        }
+
+
+        static string Show_wifi_password(string wifi_name)
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo
+            {
+                FileName = "netsh",
+                Arguments = "wlan show profile name=\"" + wifi_name + "\" key=clear",
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            Process process = new Process
+            {
+                StartInfo = startInfo
+            };
+
+            process.Start();
+            string output = process.StandardOutput.ReadToEnd();
+            process.WaitForExit();
+
+            string[] lines = output.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+            int a = 0;
+            string[] sieci = new string[lines.Length];
+
+            foreach (string line in lines)
+            {
+                if (line.Contains("    Key Content            : "))
+                {
+                    a++;
+                    string linia_1 = line.Replace("    Key Content            : ", "");
+                    sieci[0] = linia_1;
+                }
+            }
+
+            return sieci[0];
+        }
+
+        private void ShowAdvanced(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Soon feature");
         }
     }
 }
